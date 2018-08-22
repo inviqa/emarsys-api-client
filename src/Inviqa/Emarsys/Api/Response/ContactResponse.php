d54f8bbe0fd00c38aad794e888ba0d7f2b2289b8
Tim Webster <tim.webster@inviqa.com>
2018-08-22 17:03:45 +0100

    Send basic contact to Emarsys via the API

--- src/Inviqa/Emarsys/Api/Response/ContactResponse.php
+++ src/Inviqa/Emarsys/Api/Response/ContactResponse.php
@@ -4,38 +4,79 @@ namespace Inviqa\Emarsys\Api\Response;
 
 class ContactResponse
 {
-    private $isSuccessful;
-
     /**
      * @throws \LogicException
      */
-    public static function fromClientResponse(ClientResponse $response): self
+    public static function fromClientResponse(ClientResponse $clientResponse): self
     {
         $instance = new self();
+        $response = json_decode($clientResponse->getBodyContents(), true);
 
-        $instance->isSuccessful = $response->isSuccessful();
         $instance->validate($response);
+
+        $instance->replyCode = $response['replyCode'];
+        $instance->replyText = $response['replyText'];
+        $instance->data = $response['data'];
 
         return $instance;
     }
 
+    public function getReplyCode(): string
+    {
+        return $this->replyCode;
+    }
+
+    public function getReplyText(): string
+    {
+        return $this->replyText;
+    }
+
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
     public function isSuccessful(): bool
     {
-        return $this->isSuccessful;
+        return $this->replyCode == "0";
+    }
+
+    public function hasErrors()
+    {
+        return isset($this->data['errors']);
+    }
+
+    public function getErrors()
+    {
+        return $this->data['errors'];
     }
 
     /**
-     * @throws \LogicException
+     * @param mixed $response
+     *
+     * @throws \InvalidArgumentException
      */
-    private function validate(ClientResponse $response)
+    private function validate($response)
     {
-        if (!$response->isSuccessful()) {
-            throw new \LogicException(
-                sprintf(
-                    'Cannot instantiate Client Response object due an error in the request (Message: %s, Code: %s).',
-                    $response->getReasonPhrase(),
-                    $response->getStatusCode()
-                )
+        if (!is_array($response)) {
+            throw new \InvalidArgumentException('Cannot instantiate Emarsys Contact Response object due to an empty response body');
+        }
+
+        $fieldsToValidate = ['replyCode', 'replyText'];
+
+        foreach ($fieldsToValidate as $fieldName) {
+            $this->validateField($response, $fieldName);
+        }
+    }
+
+    /**
+     * @throws \InvalidArgumentException
+     */
+    private function validateField(array $response, string $fieldName)
+    {
+        if (!array_key_exists($fieldName, $response)) {
+            throw new \InvalidArgumentException(
+                sprintf('Cannot instantiate Emarsys Contact Response object because of the %s is missing from the response', $fieldName)
             );
         }
     }
